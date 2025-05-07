0cb2471ab791217618e417e826e16cd5546a2ba3
diegoodelatorre <diegoalejandrodelgadodelatorre@gmail.com>
2025-05-06 21:51:56 -0600

    Ejercicio y tarea clase 2

--- semana1/clase2_tarea.js
+++ semana1/clase2_tarea.js
@@ -10,14 +10,21 @@
 const { ask } = require('../helpers/input');
 
 async function main() {
-  const temperatura = Number(await ask("¿Cuál es la temperatura actual?"));
+  const temperatura = Number(await ask("¿Cuál es la temperatura actual? "));
 
   if (temperatura < 10) {
     console.log("Hace frío");
-  }
+  } 
   // TODO: Implementar la lógica para temperaturas entre 10 y 20 grados
   // TODO: Implementar la lógica para temperaturas entre 20 y 30 grados
   // TODO: Implementar la lógica para temperaturas superiores a 30 grados
+  else if (temperatura >= 10 && temperatura <= 20) {
+    console.log("Está templado");
+  } else if (temperatura > 20 && temperatura <= 30) {
+    console.log("Hace calor");
+  } else if (temperatura > 30) {
+    console.log("Hace mucho calor");
+  }
 
 }
 
