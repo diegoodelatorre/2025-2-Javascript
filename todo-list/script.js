3bd6cb98545a6577ef3ca7311e7a77050d2054b8
diegoodelatorre <diegoalejandrodelgadodelatorre@gmail.com>
2025-05-21 20:58:17 -0600

    prueba

--- todo-list/script.js
+++ todo-list/script.js
@@ -16,3 +16,12 @@ function addTask() {
     input.value = "";
   }
 }
+
+function deleteTask(btn){
+  btn.closest("li").remove(); // closest eliminar la etiqueta más cercana del tipo que le especifiquemos, en este caso li
+}
+
+function toggleComplete(span){
+  span.classList.toggle("task-complete"); // El toggle lo que hace es agregar una clase a la etiqueta que digamos, si ya le tiene la quita
+  // Una vez con esta clase obtiene los estilos para esa clase que hace ver que la tarea está completada
+}
